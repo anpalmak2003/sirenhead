0d36123d6fdd3483c16e2d98babe93fcc953dd96
anpalmak <fyyf0912@gmail.com>
2021-06-18 00:13:07 +0400

    Initial Commit

---
+++ chooseMonster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chooseMonster
+//
+//  Created by Maksimova Anna on 18.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chooseMonster/chooseMonsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  chooseMonsterApp.swift
+//  chooseMonster
+//
+//  Created by Maksimova Anna on 18.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct chooseMonsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
